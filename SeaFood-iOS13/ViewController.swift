ebce91acb3cb874cee27243861f0ac0e3010e807
aglegaspi <lex83nyc@gmail.com>
2020-09-11 12:51:22 -0400

    first commit - set up CoreML

---
+++ SeaFood-iOS13/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeaFood-iOS13
+//
+//  Created by Alex 6.1 on 9/11/20.
+//  Copyright © 2020 aglegaspi. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
