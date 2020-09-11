743bd0cc2634a1dd6c577ad73a6ec874854c5b95
aglegaspi <lex83nyc@gmail.com>
2020-09-11 13:05:10 -0400

    set up image picker

--- SeaFood-iOS13/ViewController.swift
+++ SeaFood-iOS13/ViewController.swift
@@ -13,11 +13,21 @@ import Vision // helps us process images more easily and use images with coreml 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageViewe: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupImagePicker()
+    }
+    
+    private func setupImagePicker() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
